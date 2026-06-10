9f24e590473b8bb9c066c417495e1b941f0ec2ed
john8815 <john8815@stthomas.edu>
2026-06-10 16:21:55 -0500

    hedaer

---
+++ scrips/animate.js
@@ -0,0 +1,20 @@
+export default function initAnimations() {
+  const targets = document.querySelectorAll("[data-animate]");
+
+  const observer = new IntersectionObserver((entries) => {
+    let delay = 0;
+    for (const entry of entries) {
+      const { isIntersecting, target } = entry;
+      if (target.getAttribute("data-animate") !== "true" && isIntersecting) {
+        setTimeout(() => {
+          target.setAttribute("data-animate", "true");
+        }, delay);
+        delay += 800;
+      }
+    }
+  });
+
+  for (const target of targets) {
+    observer.observe(target);
+  }
+}
